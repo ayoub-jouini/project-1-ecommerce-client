f4379475b0323607672f97ddb30f5f7a69eeda4e
ayoub_jouini <ayoubjouini135@gmail.com>
2022-01-12 18:00:14 +0100

    pages

---
+++ src/pages/News.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const News: React.FC = () => {
+    return (
+        <div>
+            News
+
+        </div>
+    );
+}
+
+export default News;
---
+++ src/pages/Promotions.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Promotions: React.FC = () => {
+    return (
+        <div>
+            Promotions
+        </div>
+    );
+}
+
+export default Promotions;
