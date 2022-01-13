8590e881385fd1e26ec1af16fb2d5f3a68849952
ayoub_jouini <ayoubjouini135@gmail.com>
2022-01-13 23:04:52 +0100

    createTheme, ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+
+import "./styles/app.css"
+
+import { createTheme, ThemeProvider } from '@mui/material';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AboutUs from './pages/AboutUs';
 import Blog from './pages/Blog';
@@ -10,22 +14,57 @@ import Product from './pages/Product';
 import Products from './pages/Products';
 import Promotions from './pages/Promotions';
 
+import NavBar from './components/NavBar';
+
+interface Theme {
+  palette: {
+    primary: {
+      main: string;
+      light: string;
+      dark: string;
+    }
+    secondary: {
+      main: string;
+      light: string;
+      dark: string;
+    }
+  }
+}
+
+const theme: Theme = createTheme({
+  palette: {
+    primary: {
+      main: "#30475e",
+      light: "#5c728b",
+      dark: "#032034",
+    },
+    secondary: {
+      main: "#c1a57b",
+      light: "#f5d6aa",
+      dark: "#90764e",
+    }
+  }
+});
+
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="promotions" element={<Promotions />} />
-          <Route path="blog" element={<Blog />} />
-          <Route path="news" element={<News />} />
-          <Route path="aboutus" element={<AboutUs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="products/:category" element={<Products />} />
-          <Route path="products/:category/:product" element={<Product />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
+      <ThemeProvider theme={theme}>
+        <NavBar />
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="promotions" element={<Promotions />} />
+            <Route path="blog" element={<Blog />} />
+            <Route path="news" element={<News />} />
+            <Route path="aboutus" element={<AboutUs />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="products/:category" element={<Products />} />
+            <Route path="products/:category/:product" element={<Product />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
