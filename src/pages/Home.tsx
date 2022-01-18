ee5f94967108695be165a24fc88a4599c336ab16
ayoub_jouini <ayoubjouini135@gmail.com>
2022-01-19 00:40:12 +0100

    HomePage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,56 @@
 import React from "react";
 import CarouselComponent from "../components/carousel/Carousel";
+import ProductsRow from "../components/ProductsRow.tsx/ProductsRow";
+
+const products = [
+    {
+        productName: "maryoul1",
+        productCategory: "category1",
+        image: "./assets/images/radu-florin-OP8655xgBjo-unsplash.jpg",
+        description: "maryoul behi barcha",
+        price: 60000,
+        onStock: true,
+        size: ["s", "m", "l"],
+        bestDesplay: true
+    },
+    {
+        productName: "maryoul2",
+        productCategory: "category1",
+        image: "./assets/images/afb9a61c-065a-4331-a7c1-a2b1a80b85ef.jpg",
+        description: "maryoul behi barcha",
+        price: 60000,
+        onStock: true,
+        size: ["s", "m", "l"],
+        bestDesplay: true
+    },
+    {
+        productName: "serwel1",
+        productCategory: "category2",
+        image: "./assets/images/christian-bolt-VW5VjskNXZ8-unsplash.jpg",
+        description: "maryoul behi barcha",
+        price: 60000,
+        onStock: true,
+        size: ["s", "m", "l"],
+        bestDesplay: true
+    },
+    {
+        productName: "serwel2",
+        productCategory: "category2",
+        image: "./assets/images/mahdi-bafande-6f5KtX2GGv4-unsplash.jpg",
+        description: "maryoul behi barcha",
+        price: 60000,
+        onStock: true,
+        size: ["s", "m", "l"],
+        bestDesplay: true
+    }
+]
 
 const Home: React.FC = () => {
     return (
         <div>
             <CarouselComponent />
+            <ProductsRow title="BEST-SELLER" products={products} />
+            <ProductsRow title="NEW-PRODUCTS" products={products} />
         </div>
     );
 }
