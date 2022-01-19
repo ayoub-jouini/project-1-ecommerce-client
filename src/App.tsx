03daff3787028076110dfd2de0fe70b7f63e6a44
ayoub_jouini <ayoubjouini135@gmail.com>
2022-01-20 00:26:10 +0100

    createProduct/logIn

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./styles/app.css"
 import { createTheme, ThemeProvider } from '@mui/material';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import useAuth from './utils/useAuth';
 
 import AboutUs from './pages/AboutUs';
 import Blog from './pages/Blog';
@@ -12,9 +13,13 @@ import NotFound from './pages/NotFound';
 import Product from './pages/Product';
 import Products from './pages/Products';
 import Promotions from './pages/Promotions';
+import LogIn from './pages/LogIn';
+import CreateProduct from './pages/CreateProduct';
 
 import NavBar from './components/navBar/NavBar';
 import Footer from './components/footer/Footer';
+
+import AuthContext from './utils/auth-context';
 
 interface Theme {
   palette: {
@@ -47,23 +52,41 @@ const theme: Theme = createTheme({
 });
 
 const App: React.FC = () => {
+
+  const { token, login, logout, userId, emailT } = useAuth();
+
+  let dashboardRoute = <LogIn />;
+  if (token) {
+    dashboardRoute = (<CreateProduct />)
+  }
+
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <div className="App">
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="promotions" element={<Promotions />} />
-            <Route path="blog" element={<Blog />} />
-            <Route path="news" element={<News />} />
-            <Route path="aboutus" element={<AboutUs />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="products/:category" element={<Products />} />
-            <Route path="products/:category/:product" element={<Product />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-          <Footer />
+          <AuthContext.Provider value={{
+            isLoggedIn: !!token,
+            userId: userId,
+            email: emailT,
+            token: token,
+            logIn: login,
+            logOut: logout
+          }}>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="promotions" element={<Promotions />} />
+              <Route path="blog" element={<Blog />} />
+              <Route path="news" element={<News />} />
+              <Route path="aboutus" element={<AboutUs />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="products/:category" element={<Products />} />
+              <Route path="products/:category/:product" element={<Product />} />
+              <Route path="admin-dashboard" element={dashboardRoute} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+            <Footer />
+          </AuthContext.Provider>
         </div>
       </ThemeProvider>
     </BrowserRouter>
