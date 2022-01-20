8fc666e29f4cb8cde70a8ef9bca6da46e25f6735
ayoub_jouini <ayoubjouini135@gmail.com>
2022-01-20 20:35:59 +0100

    homePage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,57 +1,33 @@
+import { Box } from "@mui/material";
+import axios from "axios";
 import React from "react";
+import { useQuery } from "react-query";
 import CarouselComponent from "../components/carousel/Carousel";
+import LoadingContainer from "../components/loadingProduct.tsx/LoadingContainer";
+import LoadingError from "../components/loadingProduct.tsx/LoadingError";
+import NewsLetter from "../components/newsLetter/NewsLetter";
 import ProductsRow from "../components/ProductsRow.tsx/ProductsRow";
 
-const products = [
-    {
-        productName: "maryoul1",
-        productCategory: "category1",
-        image: "./assets/images/radu-florin-OP8655xgBjo-unsplash.jpg",
-        description: "maryoul behi barcha",
-        price: 60000,
-        onStock: true,
-        size: ["s", "m", "l"],
-        bestDesplay: true
-    },
-    {
-        productName: "maryoul2",
-        productCategory: "category1",
-        image: "./assets/images/afb9a61c-065a-4331-a7c1-a2b1a80b85ef.jpg",
-        description: "maryoul behi barcha",
-        price: 60000,
-        onStock: true,
-        size: ["s", "m", "l"],
-        bestDesplay: true
-    },
-    {
-        productName: "serwel1",
-        productCategory: "category2",
-        image: "./assets/images/christian-bolt-VW5VjskNXZ8-unsplash.jpg",
-        description: "maryoul behi barcha",
-        price: 60000,
-        onStock: true,
-        size: ["s", "m", "l"],
-        bestDesplay: true
-    },
-    {
-        productName: "serwel2",
-        productCategory: "category2",
-        image: "./assets/images/mahdi-bafande-6f5KtX2GGv4-unsplash.jpg",
-        description: "maryoul behi barcha",
-        price: 60000,
-        onStock: true,
-        size: ["s", "m", "l"],
-        bestDesplay: true
-    }
-]
-
 const Home: React.FC = () => {
+
+    const bestProductData = useQuery(['products', 'best'], async () => {
+        const data = await axios.get('http://localhost:5000/api/products/bestProducts')
+        return data;
+    });
+
     return (
-        <div>
+        <Box sx={{ marginDown: "100px" }}>
             <CarouselComponent />
-            <ProductsRow title="BEST-SELLER" products={products} />
-            <ProductsRow title="NEW-PRODUCTS" products={products} />
-        </div>
+            {bestProductData.isLoading && <LoadingContainer title="BEST-Seller" />}
+            {bestProductData.isError && <LoadingError title="BEST-SELLER" />}
+            {bestProductData.data && <ProductsRow title="BEST-SELLER" products={bestProductData.data?.data.bestProducts} />}
+
+            {bestProductData.isLoading && <LoadingContainer title="NEW-PRODUCTS" />}
+            {bestProductData.isError && <LoadingError title="NEW-PRODUCTS" />}
+            {bestProductData.data && <ProductsRow title="NEW-PRODUCTS" products={bestProductData.data?.data.bestProducts} />}
+
+            <NewsLetter />
+        </Box>
     );
 }
 
