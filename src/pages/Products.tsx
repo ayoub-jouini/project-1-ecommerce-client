2ca7ebc3e4bd0d33beb7101dd3792a9ff06229c0
ayoub_jouini <ayoubjouini135@gmail.com>
2022-01-24 23:38:35 +0100

    Products

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,10 +1,23 @@
+import { Box } from "@mui/system";
 import React from "react";
+import { useQueryClient } from "react-query";
+import SideBar from "../components/sideBar/SideBar";
 
 const Products: React.FC = () => {
+
+    const queryClient = useQueryClient()
+    const data = queryClient.getQueriesData(['products', 'best']);
+    console.log(data)
+
     return (
-        <div>
-            Products
-        </div>
+        <Box>
+            <Box sx={{ width: "20%" }}>
+                {/* <SideBar /> */}
+            </Box>
+            <Box>
+
+            </Box>
+        </Box>
     );
 }
 
