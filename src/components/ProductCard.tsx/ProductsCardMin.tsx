baa53899480116bf8f4ca44b625fab4b3e5aa008
ayoub_jouini <ayoubjouini135@gmail.com>
2022-01-25 14:01:30 +0100

    ProductCardMin

--- src/components/ProductCard.tsx/ProductsCardMin.tsx
+++ src/components/ProductCard.tsx/ProductsCardMin.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, CardMedia, Typography } from "@mui/material";
 import React from "react";
 
 interface Props {
@@ -11,21 +11,28 @@ interface Props {
 
 const ProductCardMin: React.FC<Props> = ({ name, description, price, category, image }) => {
     return (
-        <Card sx={{ display: 'flex' }}>
+        <Card sx={{ display: 'flex', height: "6rem", marginTop: "1rem" }}>
             <CardMedia
                 component="img"
-                sx={{ width: 151 }}
-                image={image}
+                sx={{ width: "40%" }}
+                image={`http://localhost:5000/${image}`}
                 alt={image}
             />
             <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                <CardContent sx={{ flex: '1 0 auto' }}>
-                    <Typography component="div" variant="h5">
-                        {name}
-                    </Typography>
-                    <Typography variant="subtitle1" color="text.secondary" component="div">
-                        {price}
-                    </Typography>
+                <CardContent sx={{ flex: '1 0 auto', display: "flex" }}>
+                    <Box sx={{ width: "70%" }}>
+                        <Typography component="div" variant="h6">
+                            {name}
+                        </Typography>
+                        <Typography variant="body1" color="text.secondary" component="div">
+                            {price}
+                        </Typography>
+                    </Box>
+                    <Box sx={{ width: "20%", height: "100%" }}>
+                        <Button sx={{ width: "100%", height: "100%" }} >
+                            Buy
+                        </Button>
+                    </Box>
                 </CardContent>
             </Box>
 
