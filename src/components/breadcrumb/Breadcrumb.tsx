ccaa00d8a5d7d146b5186630e14054f250f45e30
ayoub_jouini <ayoubjouini135@gmail.com>
2022-01-25 23:45:11 +0100

    breadcrumb

---
+++ src/components/breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,52 @@
+import { Breadcrumbs, Container, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+
+const Breadcrumb: React.FC = () => {
+
+    const params = useParams();
+
+    return (
+        <Box sx={{
+            height: "1.5rem",
+            padding: "2rem",
+            backgroundColor: "primary.light",
+            color: "#ECECEC"
+        }}>
+            <Container sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: { xs: "center", md: "space-between" }
+            }}>
+                <Box sx={{ display: { xs: "none", md: "flex" } }}>
+                    <Typography variant="h5">
+                        {params.category}
+                    </Typography>
+                </Box>
+                <Breadcrumbs aria-label="breadcrumb">
+                    <Link to="/">
+                        <Typography variant="body1" color="#ECECEC">
+                            Home
+                        </Typography>
+                    </Link>
+                    <Link
+                        to=""
+                    >
+                        <Typography variant="body1" color="#ECECEC">
+                            Products
+                        </Typography>
+                    </Link>
+                    <Link to="">
+                        <Typography variant="body1" color="#ECECEC">
+                            {params.category}
+                        </Typography>
+                    </Link>
+                </Breadcrumbs>
+            </Container>
+        </Box>
+
+    );
+}
+
+export default Breadcrumb;
