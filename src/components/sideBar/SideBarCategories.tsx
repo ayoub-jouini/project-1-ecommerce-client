82e7619422adcd8c7244262d431125b7d884053e
ayoub_jouini <ayoubjouini135@gmail.com>
2022-01-26 00:42:54 +0100

    sideBarCategories

---
+++ src/components/sideBar/SideBarCategories.tsx
@@ -0,0 +1,25 @@
+import { Box, Button } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+
+interface Props {
+    categories: any
+}
+
+const SideBarCategories: React.FC<Props> = ({ categories }) => {
+    return (
+        <Box>
+            {
+                categories.map((category: any, key: any) => (
+                    <Link to={`/products/${category}`} key={key} >
+                        <Button sx={{ display: 'block', width: '100%', textAlign: 'left', height: "3rem" }} color="primary">
+                            {category.categoryName}
+                        </Button>
+                    </Link>
+                ))
+            }
+        </Box>
+    );
+}
+
+export default SideBarCategories;
