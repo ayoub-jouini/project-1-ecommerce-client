dd9c09c755d465a086d2d25792a4666161fe1e65
ayoub_jouini <ayoubjouini135@gmail.com>
2022-01-26 14:49:32 +0100

    LoadingError

--- src/components/loadingProduct.tsx/LoadingError.tsx
+++ src/components/loadingProduct.tsx/LoadingError.tsx
@@ -1,38 +1,19 @@
 import React from "react";
 import ErrorIcon from '@mui/icons-material/Error';
-import { Container, styled, Typography, Box } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 
-const BoxStyle = styled('div')(({ theme }) => ({
-    padding: theme.spacing(1),
-    [theme.breakpoints.up('sm')]: {
-        height: "550px",
-    },
-    [theme.breakpoints.down('sm')]: {
-        height: "2050px",
-    }
-}));
 
-interface Props {
-    title: string;
-}
 
-const LoadingError: React.FC<Props> = ({ title }) => {
+const LoadingError: React.FC = () => {
     return (
-        <Container sx={{ marginTop: "50px", marginBottom: "50px" }} >
-            <BoxStyle>
-                <Box sx={{ width: "100%", textAlign: "center", marginBottom: "50px" }} >
-                    <Typography variant='h4' color="primary">{title}</Typography>
-                </Box>
-                <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <Box >
-                        <ErrorIcon fontSize="large" color="primary" sx={{ height: 80 }} />
-                    </Box>
-                    <Box sx={{ width: "100%", textAlign: "center", marginBottom: "50px" }} >
-                        <Typography variant='h5' color="primary">Sorry, Something went wrong</Typography>
-                    </Box>
-                </Box>
-            </BoxStyle>
-        </Container>
+        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Box >
+                <ErrorIcon fontSize="large" color="primary" sx={{ height: 80 }} />
+            </Box>
+            <Box sx={{ width: "100%", textAlign: "center", marginBottom: "50px" }} >
+                <Typography variant='h5' color="primary">Sorry, Something went wrong</Typography>
+            </Box>
+        </Box>
     );
 }
 
