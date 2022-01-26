c4a39211b5c41ab5746d84690ef50212cd3836ad
ayoub_jouini <ayoubjouini135@gmail.com>
2022-01-26 14:48:59 +0100

    productsContainer

---
+++ src/components/ProductsContainer/ProductsContainer.tsx
@@ -0,0 +1,51 @@
+import { Box, Grid } from "@mui/material";
+import axios from "axios";
+import React from "react";
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import LoadingContainer from "../loadingProduct.tsx/LoadingContainer";
+import LoadingError from "../loadingProduct.tsx/LoadingError";
+import ProductCard from "../ProductCard.tsx/ProductCard";
+
+interface Props {
+
+}
+
+const ProductsContainer: React.FC<Props> = ({ }) => {
+
+    const params = useParams();
+
+    const category = params.category || "";
+    const products = useQuery(['products', category], async () => {
+        const data = await axios.get(`http://localhost:5000/api/products/${category}`);
+        return data;
+    })
+
+    return (
+        <Box margin="1.5rem 0" >
+            {
+                products.isLoading && <LoadingContainer />
+            }
+            {
+                products.isError && <LoadingError />
+            }
+            <Grid container spacing={7} >
+                {products.data?.data.products &&
+                    products.data?.data.products.map((product: any, key: any) => (
+                        <Grid item xs={12} sm={4} key={key}>
+                            <ProductCard
+                                name={product.productName}
+                                description={product.description}
+                                category={product.productCategory}
+                                price={product.price}
+                                image={product.image}
+                            />
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        </Box>
+    );
+}
+
+export default ProductsContainer;
