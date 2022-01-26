da195ac588cb551bc92f5653290d1963e22660b5
ayoub_jouini <ayoubjouini135@gmail.com>
2022-01-26 14:50:19 +0100

    home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,22 @@
-import { Box } from "@mui/material";
+import { Box, Container, styled, Typography } from "@mui/material";
 import axios from "axios";
 import React from "react";
 import { useQuery } from "react-query";
 import CarouselComponent from "../components/carousel/Carousel";
-import LoadingContainer from "../components/loadingProduct.tsx/LoadingContainer";
+import LoadingRow from "../components/loadingProduct.tsx/LoadingRow";
 import LoadingError from "../components/loadingProduct.tsx/LoadingError";
 import NewsLetter from "../components/newsLetter/NewsLetter";
 import ProductsRow from "../components/ProductsContainer/ProductsRow";
+
+const BoxStyle = styled('div')(({ theme }) => ({
+    padding: theme.spacing(1),
+    [theme.breakpoints.up('sm')]: {
+        height: "550px",
+    },
+    [theme.breakpoints.down('sm')]: {
+        height: "2050px",
+    }
+}));
 
 const Home: React.FC = () => {
 
@@ -27,14 +37,28 @@ const Home: React.FC = () => {
     return (
         <Box sx={{ marginDown: "100px" }}>
             <CarouselComponent />
-            {bestProductData.isLoading && <LoadingContainer title="BEST-SELLER" />}
-            {bestProductData.isError && <LoadingError title="BEST-SELLER" />}
-            {bestProductData.data && <ProductsRow title="BEST-SELLER" products={sliceData(bestProductData.data?.data.bestProducts)} />}
-
-            {newProductsData.isLoading && <LoadingContainer title="NEW-PRODUCTS" />}
-            {newProductsData.isError && <LoadingError title="NEW-PRODUCTS" />}
-            {newProductsData.data && <ProductsRow title="NEW-PRODUCTS" products={sliceData(newProductsData.data?.data.newProducts)} />}
-
+            <Container sx={{ marginTop: "50px", marginBottom: "50px" }} >
+                <BoxStyle>
+                    <Box sx={{ width: "100%", textAlign: "center", marginBottom: "50px" }} >
+                        <Typography variant='h4' color="primary">BEST-SELLER</Typography>
+                    </Box>
+                    <Box>
+                        {bestProductData.isLoading && <LoadingRow />}
+                        {bestProductData.isError && <LoadingError />}
+                        {bestProductData.data && <ProductsRow products={sliceData(bestProductData.data?.data.bestProducts)} />}
+                    </Box>
+                </BoxStyle>
+            </Container>
+            <Container sx={{ marginTop: "50px", marginBottom: "50px" }} >
+                <BoxStyle>
+                    <Box sx={{ width: "100%", textAlign: "center", marginBottom: "50px" }} >
+                        <Typography variant='h4' color="primary">NEW-Products</Typography>
+                    </Box>
+                    {newProductsData.isLoading && <LoadingRow />}
+                    {newProductsData.isError && <LoadingError />}
+                    {newProductsData.data && <ProductsRow products={sliceData(newProductsData.data?.data.newProducts)} />}
+                </BoxStyle>
+            </Container>
             <NewsLetter />
         </Box>
     );
