8037dcecfcb3a49f1c8249660a1bdb3001a983cf
ayoub_jouini <ayoubjouini135@gmail.com>
2022-01-26 14:50:09 +0100

    products page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,6 +1,7 @@
 import { Container } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
+import { Outlet } from "react-router-dom";
 import Breadcrumb from "../components/breadcrumb/Breadcrumb";
 import SideBar from "../components/sideBar/SideBar";
 
@@ -10,13 +11,13 @@ const Products: React.FC = () => {
         <Box sx={{ width: "100%" }}>
 
             <Breadcrumb />
-            <Container>
+            <Container sx={{ display: "flex", justifyContent: "space-between" }}>
 
                 <Box sx={{ width: "25%", display: { xs: 'none', md: 'flex' } }}>
                     <SideBar />
                 </Box>
-                <Box>
-
+                <Box sx={{ width: { xs: "100%", md: "72%" } }} >
+                    <Outlet />
                 </Box>
             </Container>
         </Box >
