a03013088b571817545a83ff8d5e7ac7b8f49a6a
ayoub_jouini <ayoubjouini135@gmail.com>
2022-03-08 12:40:25 +0100

    contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import { Box, Button, Card, CardActionArea, CardMedia, Container, Grid, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import Breadcrumb from "../components/breadcrumb/Breadcrumb";
+import ContactForm from "../components/contact/ContactForm";
 
 const Contact: React.FC = () => {
     return (
-        <div>
-            Contact
-        </div>
+        <Box>
+            <Breadcrumb pageName="Contact" />
+            <ContactForm />
+        </Box>
     );
 }
 
