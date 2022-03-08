3d213f1386ca4f3c27941593e2c8b9e7e3c1b353
ayoub_jouini <ayoubjouini135@gmail.com>
2022-03-08 19:01:35 +0100

    NotFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,19 @@
+import { Box, Container, Typography } from "@mui/material";
+import ErrorIcon from '@mui/icons-material/Error';
 import React from "react";
 
 const NotFound: React.FC = () => {
     return (
-        <div>
-            NotFound
-        </div>
+        <Box sx={{ height: "460px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <Container sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <Box >
+                    <ErrorIcon color="primary" sx={{ height: 80, fontSize: "50px" }} />
+                </Box>
+                <Box sx={{ width: "100%", textAlign: "center", marginBottom: "50px" }} >
+                    <Typography variant='h5' color="primary">Sorry, could not find this page</Typography>
+                </Box>
+            </Container>
+        </Box >
     );
 }
 
