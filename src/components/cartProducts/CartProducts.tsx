bd80bd4292f5ad0582c39b37ea9ce89f549e457f
ayoub_jouini <ayoubjouini135@gmail.com>
2022-03-12 23:57:44 +0100

    CartProducts

--- src/components/cartProducts/CartProducts.tsx
+++ src/components/cartProducts/CartProducts.tsx
@@ -1,31 +1,31 @@
 import { Box } from '@mui/material';
-import React, { useContext, useState } from 'react'
-import CartContext from '../../utils/Cart-context';
+import React, { useContext } from 'react'
+import { useCart } from '../../utils/Cart-Context';
 import CartCard from './CartCard';
 
 const CartProducts: React.FC = () => {
 
-    const products = useContext(CartContext);
+    const { cart, addToCart, removeFromCart } = useCart()
+
     return (
         <Box>
-            {/* {products.products.map((product: any, key: any) => (
+            {cart.map((product: any, key: any) => (
                 <CartCard
                     key={key}
                     productImage={product.image}
                     productName={product.name}
-                    productCategory={product.category}
-                    productDescription={product.description}
                     productSize={product.size}
+                    productAmount={product.amount}
                     productPrice={product.price} />
-            ))} */}
-            <CartCard productImage='./assets/images/radu-florin-OP8655xgBjo-unsplash.jpg'
+            ))}
+            {/* <CartCard productImage='./assets/images/radu-florin-OP8655xgBjo-unsplash.jpg'
                 productName='mariyoul'
                 productCategory='category1'
                 productDescription='qdfsdfsdfsdfdfsfs'
                 productSize='m'
                 productAmount={2}
                 productPrice={40}
-            />
+            /> */}
         </Box>
     );
 }
